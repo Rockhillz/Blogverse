4b0f56aa73be708c47b60ecce24476c5b07b8a03
Izuchukwu Alaneme <izuchi.alaneme@gmail.com>
2025-03-02 20:24:48 +0100

    Updated cors with frontend url

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "",
+      "https://web-blogverse.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
